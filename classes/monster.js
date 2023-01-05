24da57da904f39a4fe568d4ae1c06efaa4ecb0ff
kulbil <martin.austvold@gmail.com>
2023-01-06 00:04:05 +0100

    first commit

---
+++ classes/monster.js
@@ -0,0 +1,7 @@
+class monster {
+    constructor(name, strength, health) {
+        this.name = name;
+        this.strength = strength;
+        this.health = health;
+    }
+}
---
+++ script.js
@@ -0,0 +1,13 @@
+var weaponList = [];
+weaponList.push(new weapon("Stick", 5));
+weaponList.push(new weapon("Axe", 10));
+weaponList.push(new weapon("Spear", 15));
+
+var monsterList = [];
+monsterList.push(new monster("Garmadon", 24, 5));
+monsterList.push(new monster("SanchayDenSure", 10, 4));
+monsterList.push(new monster("Jwoodh", 13, 8));
+
+console.log(monsterList[0].name);
+
+console.log(room1.monster.name);
