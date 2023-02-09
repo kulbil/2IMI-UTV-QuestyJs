75a521261259125a63c50b8ad716601d99da41a5
kulbil <martin.austvold@gmail.com>
2023-02-09 13:06:59 +0100

    aded custom font and finished admin page

--- QuestyjsCode/admin.php
+++ QuestyjsCode/admin.php
@@ -21,19 +21,19 @@
             <div id="adminListContainer">
                 <input type="text" id="userSearch" placeholder="Search for username">
                 <div id="userList">
-                    <table id="tableHeader">
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Username</th>
-                        <th>Highscore</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </table>
                     <table id="dbTable">
                         <?php
+                            echo "<tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>Username</th>
+                                <th>Highscore</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                                </tr>";
                             $sql = "SELECT * FROM users WHERE NOT rank='admin'";
                             $result = $conn->query($sql);
-                            while ($row = $result -> fetch_assoc()) {
+                            while ($row = $result -> fetch_assoc()) { 
                                 echo "<tr>
                                     <td>".$row['id']."</td>
                                     <td>".$row['name']."</td>
@@ -41,6 +41,7 @@
                                     <td>".$row['highscore']."</td>
                                     <td>".$row['rank']."</td>
                                     <td class='buttonColumn'><button class='banBtn ".$row['rank']."' id=".$row['id']." value=".$row['rank']."></button></td>
+                                    <td class='buttonColumn'><button class='deleteBtn' id=".$row['id']."></button></td>
                                     </tr>";
                             };
                         ?>
@@ -57,7 +58,18 @@
             console.log(this.id);
             $.post("includes/admin.inc.php", {
                 selectedRowId: this.id,
-                selectedRowRank: this.value
+                selectedRowRank: this.value,
+                buttonPressed: "ban"
+            }, function(data) {
+                $("#dbTable").html(data);
+            });
+        });
+
+        $("#dbTable").on("click", ".deleteBtn", function(){
+            console.log(this.id);
+            $.post("includes/admin.inc.php", {
+                selectedRowId: this.id,
+                buttonPressed: "delete"
             }, function(data) {
                 $("#dbTable").html(data);
             });
