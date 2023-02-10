18939016e5b33a69ab48aa2affc5d12c41f5e4cf
kulbil <martin.austvold@gmail.com>
2023-02-10 12:50:52 +0100

    faq stzart

--- QuestyjsCode/faq.php
+++ QuestyjsCode/faq.php
@@ -2,7 +2,21 @@
     include_once 'header.php';
 ?>
 
-    FAQ
+<div id="faqContainer">
+    <div id="faqQuestion1">
+        <div id="faqQ1">
+            <h1 >
+                Hvordan har du det?
+            </h1>
+        </div>
+        <div id="faqA1">
+            <p id="faqA1">
+                jeg har det veldig bra takk for at du spør :)
+            </p>
+        </div>
+    </div>
+</div>
+
 
 <?php
     include_once 'footer.php';
