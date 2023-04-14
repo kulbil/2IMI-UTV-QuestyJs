e6f23438d80fdbae028c79ebc35f1d937803ff31
kulbil <martin.austvold@gmail.com>
2023-04-14 11:36:11 +0200

    updated faq

--- QuestyjsCode/faq.php
+++ QuestyjsCode/faq.php
@@ -4,18 +4,55 @@
 
 <div id="blob"></div>
 <div id="faqContainer">
-    <div class="faqQuestion">
-        <div id="faqQ1">
-            <p>
-                Hvordan har du det?
-            </p>
-        </div>
-        <div id="faqA1">
-            <p>
-                jeg har det veldig bra takk for at du spør :)
-            </p>
-        </div>
+    <div class="faqQ">
+        <p class="faqQText">
+            Hvordan har du det?
+        </p>
+        <p class="faqA">
+            jeg har det veldig bra takk for at du spør :)
+        </p>
     </div>
+    <div class="faqQ">
+        <p class="faqQText">
+            Hvordan har du det?
+        </p>
+        <p class="faqA">
+            jeg har det veldig bra takk for at du spør :)
+        </p>
+    </div>
+    <div class="faqQ">
+        <p class="faqQText">
+            Hvordan har du det?
+        </p>
+        <p class="faqA">
+            jeg har det veldig bra takk for at du spør :)
+        </p>
+    </div>
+    <div class="faqQ">
+        <p class="faqQText">
+            Hvordan har du det?
+        </p>
+        <p class="faqA">
+            jeg har det veldig bra takk for at du spør :)
+        </p>
+    </div>
+    <div class="faqQ">
+        <p class="faqQText">
+            Hvordan har du det?
+        </p>
+        <p class="faqA">
+            jeg har det veldig bra takk for at du spør :)
+        </p>
+    </div>
+    <div class="faqQ">
+        <p class="faqQText">
+            Hvordan har du det?
+        </p>
+        <p class="faqA">
+            jeg har det veldig bra takk for at du spør :)
+        </p>
+    </div>
+    
 </div>
 <script src="faqscript.js"></script>
 </body>
